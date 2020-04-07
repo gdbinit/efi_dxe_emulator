40d03a2b2a77367735bf99d2b70eb535d72761f7
Assaf Carlsbad <carlsbad@sentinelone.com>
2020-04-07 19:26:22 +0300

    Supply correct arguments to notification routine.

--- efi_dxe_emulator/events.cpp
+++ efi_dxe_emulator/events.cpp
@@ -8,6 +8,8 @@
 struct EFI_EVENT_IMPL
 {
     bool signaled;
+    uint64_t notify_routine;
+    uint64_t notify_context;
     /* Trampoline for the notification routine */
     uint64_t tramp[2];
 };
@@ -18,8 +20,11 @@ EFI_EVENT create_efi_event(uc_engine * uc, UINT32 Type, EFI_TPL NotifyTpl, EFI_E
 {
     static uint64_t g_counter = 1;
 
-    EFI_EVENT_IMPL ei = { false, 0, 0 };
-    install_trampoline(uc, reinterpret_cast<uint64_t>(NotifyFunction), &ei.tramp[0], &ei.tramp[1]);
+    auto notify_routine = reinterpret_cast<uint64_t>(NotifyFunction);
+    auto notify_context = reinterpret_cast<uint64_t>(NotifyContext);
+
+    EFI_EVENT_IMPL ei = { false, notify_routine, notify_context, 0, 0 };
+    install_trampoline(uc, notify_routine, &ei.tramp[0], &ei.tramp[1]);
     
     g_events[reinterpret_cast<EFI_EVENT>(g_counter)] = ei;
     return reinterpret_cast<EFI_EVENT>(g_counter++);
@@ -37,7 +42,9 @@ void dispatch_event_notification_routines(uc_engine* uc)
     {
         if (ei.second.signaled)
         {
-            DEBUG_MSG("Dispatching notification routine for event %p at %p", ei.first, ei.second.tramp[0]);
+            DEBUG_MSG("Dispatching notification routine for event %p at %p", ei.first, ei.second.notify_routine);
+            uc_reg_write(uc, UC_X86_REG_RCX, &ei.first);
+            uc_reg_write(uc, UC_X86_REG_RDX, &ei.second.notify_context);
             uc_emu_start(uc, ei.second.tramp[0], ei.second.tramp[1], 0, 0);
         }
     }
