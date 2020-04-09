ea9313887e86614d40a23e7620a7f28b7e734178
Assaf Carlsbad <carlsbad@sentinelone.com>
2020-04-10 01:33:44 +0300

    Use wchar_t instead of CHAR16 to get a friendlier view of strings in the VS debugger.

--- efi_dxe_emulator/efi_runtime_hooks.cpp
+++ efi_dxe_emulator/efi_runtime_hooks.cpp
@@ -326,7 +326,7 @@ hook_GetVariable(uc_engine *uc, uint64_t address, uint32_t size, void *user_data
     
     /* XXX: max 256 wide chars */
     /* we have no idea about the length and it's residing on Unicorn memory */
-    CHAR16 VariableName[256+1] = {0};
+    wchar_t VariableName[256+1] = {0};
     EFI_GUID VendorGuid = {0};
     uint32_t Attributes = 0;
     uint32_t DataSize = 0;
